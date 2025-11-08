9ea61ca24134f2d142b9eed42202051dd5502dd6
John Robin <johnrobin044@gmail.com>
2025-11-08 19:12:26 +0000

    Refresh .gitignore and untrack files

--- build/CMakeFiles/OC_Relay.elf.dir/stm32f4xx-hal-driver/Src/stm32f4xx_ll_fmpi2c.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/OC_Relay.elf.dir/stm32f4xx-hal-driver/Src/stm32f4xx_ll_fmpi2c.c.obj: \
- /home/johnr/Devel/PS_assign/stm32f4xx-hal-driver/Src/stm32f4xx_ll_fmpi2c.c
--- build/CMakeFiles/OC_Relay.elf.dir/stm32f4xx-hal-driver/Src/stm32f4xx_ll_spi.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/OC_Relay.elf.dir/stm32f4xx-hal-driver/Src/stm32f4xx_ll_spi.c.obj: \
- /home/johnr/Devel/PS_assign/stm32f4xx-hal-driver/Src/stm32f4xx_ll_spi.c
